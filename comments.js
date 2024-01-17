46a7f775ff0546ef7e9bc346d480423f38efb0ec
Wael Mokhallalate <145333411+WyLr99@users.noreply.github.com>
2024-01-17 11:59:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/comments', (req, res) => {
-    res.send('GET: comments');
-    });
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+        },
+        {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+        },
+        {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+        },
+        {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+        }
+];
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
