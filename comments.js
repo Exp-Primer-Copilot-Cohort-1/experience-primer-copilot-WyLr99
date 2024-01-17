820d2d9c4e63c2246699772aafda78aaa527091e
Wael Mokhallalate <145333411+WyLr99@users.noreply.github.com>
2024-01-17 11:57:35 +0000

    please work

--- comments.js
+++ comments.js
@@ -1,6 +1,11 @@
-//create web server
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path');
-const bodyParser = require('body-parser');
+
+app.get('/comments', (req, res) => {
+    res.send('GET: comments');
+    });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
